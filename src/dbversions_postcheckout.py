2933bc79f90dfd0db1c217895eba216aaaa160bc
chof747 <chof@gmx.at>
2018-05-05 18:20:56 +0200

    Changed path finding of checkout script

--- src/dbversions_postcheckout.py
+++ src/dbversions_postcheckout.py
@@ -11,8 +11,9 @@ if (branchCheckout == 1) and not (prevHead == newHead):
     import os
     from dbversions import Config, DBConfig
     
-    projectpath = os.path.dirname(os.path.dirname(os.path.realpath(__file__))+'/../../')
+    projectpath = os.getcwd()
     cfg = Config(projectpath)
+    cfg.setLoggingVerbosity(2)
     dbconfig = DBConfig(cfg)
     dbconfig.checkout()
     
